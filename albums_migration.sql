b3e30755c2e0eedbc574679f5ae380604339f85d
Ruusa Bolton <ruusabolton@Ruusas-MacBook-Pro.local>
2018-08-11 14:36:22 -0500

    create db, user and give permissions

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,4 +1,7 @@
-USE codeup_test_db;
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
+SHOW DATABASES;
 
+CREATE USER 'codeup_test_user'@'localhost' IDENTIFIED BY 'open1973';
+SELECT user from mysql.user;
 
-show create table albums;
+GRANT ALL ON codeup_test_db.* TO 'codeup_test_user'@'localhost';
