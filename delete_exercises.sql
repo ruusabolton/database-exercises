6ff1f26bfef9e2717acb684cb719f6eb487ac6cf
Ruusa Bolton <ruusabolton@Ruusas-MacBook-Pro.local>
2018-08-11 18:22:57 -0500

    deleted rows

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,7 +1,18 @@
 USE codeup_test_db;
+
+/* albums released after 1991*/
+
+DELETE
+FROM albums
+WHERE release_date > 1991;
+
+/* Albums where genre is disco*/
+DELETE
+FROM albums
+WHERE genre = 'disco';
+
 /* albums by Whitney Houston*/
 
-SELECT *
+DELETE
 FROM albums
 WHERE artist = 'Whitney Houston';
-
